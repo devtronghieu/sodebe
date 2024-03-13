4e7492996c1605e1007240e7caf3a877fab9923a
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-03-13 17:27:54 +0700

    chore: load nest config & mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
@@ -8,6 +9,7 @@ import { CountersModule } from './counters/counters.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
