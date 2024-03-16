7c1eda5bb7963049be251c2687e587cf9a9baf01
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-03-16 23:43:45 +0700

    chore: remove hydrated document type

--- src/counters/entities/counter.entity.ts
+++ src/counters/entities/counter.entity.ts
@@ -1,6 +1,5 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
 
 @Schema()
 @ObjectType()
@@ -13,7 +12,5 @@ export class Counter {
   @Field(() => Int, { description: 'Count (start from 0)' })
   count: number;
 }
-
-export type CounterDocument = HydratedDocument<Counter>;
 
 export const CounterSchema = SchemaFactory.createForClass(Counter);
